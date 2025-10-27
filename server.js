295da9ed5c8c443bd06623366bd364cf6059f8e9
Mus'ab Anas <Musabanas99@gmail.com>
2025-10-27 16:08:58 +0800

    edit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const DATA_FILE = "./data/places.json";
+const DATA_FILE = "./places.json";
 
 // Get all data
 app.get("/places", (req, res) => {
